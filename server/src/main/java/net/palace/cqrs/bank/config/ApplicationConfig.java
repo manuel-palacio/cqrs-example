85c0db3d0b9d8f443a6aaba3fcffb9ee9728050f
Manuel Palacio <emanuel.palacio@gmail.com>
2012-04-22 02:03:40 +0200

    added configuration class

--- server/src/main/java/net/palace/cqrs/bank/config/ApplicationConfig.java
+++ server/src/main/java/net/palace/cqrs/bank/config/ApplicationConfig.java
@@ -1,26 +1,10 @@
 package net.palace.cqrs.bank.config;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.MultiMap;
-import net.palace.cqrs.bank.Money;
-import net.palace.cqrs.bank.Transaction;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-
-import java.util.Map;
 
 @Configuration
 @ImportResource("classpath*:application-context.xml")
 public class ApplicationConfig {
 
-    @Bean(name = "transactions")
-    public MultiMap<String, Transaction> transactions() {
-        return Hazelcast.getMultiMap("transactions");
-    }
-
-    @Bean(name = "accounts")
-    public Map<String, Money> accounts() {
-        return Hazelcast.getMap("accounts");
-    }
 }
